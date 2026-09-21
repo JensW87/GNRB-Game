87aaed556cfb837243858ca6a1ef72c47a8d67fc
JensW87 <jensplagemann@gmx.de>
2026-09-21 13:02:00 +0200

    zelda3decomp

--- GNRB-Game/engine/scripts/build.bat
+++ GNRB-Game/engine/scripts/build.bat
@@ -3,7 +3,7 @@ echo ==========================
 echo Building GNRB...
 echo ==========================
 
-"C:\Users\Anwender\Desktop\GNRBGame\asar\asar.exe" "..\asm\main.asm" "..\..\builds\GNRB.sfc"
+"C:\Users\Anwender\Desktop\GNRBGame\asar\asar.exe" "..\asm\main.asm" "..\..\builds\alttp_us.sfc"
 
 echo.
 echo Build finished.
